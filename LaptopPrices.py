c3d71da709259b5789526ce316d4847ecf0d250a
devarajphukan <devaraj.phukan@gmail.com>
2015-09-13 20:05:08 +0530

    Create LaptopPrices.py

---
+++ LaptopPrices.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import threading
+import requests
+from bs4 import BeautifulSoup
+
+def spider(i):
+
+	url = "http://www.flipkart.com/lc/pr/pv1/spotList1/spot1/productList?sid=6bo%2Cb5g&filterNone=true&start=" + str(i) + "&ajax=true&_=1442149456627"
+
+	html_text = requests.get(url)
+
+	plain_text = html_text.text
+	soup = BeautifulSoup(plain_text)
+	
+	try :
+	
+		for j in soup.find_all("div",{"class":"pu-details lastUnit"}):
+
+			k = j.find("a",{"class":"fk-display-block"}).get("title")
+			k = str(k).split("(")[0]
+			
+			l = j.find("span",{"class":"fk-font-17 fk-bold"}).text
+			
+			print str(k)
+			print str(l)
+	
+	except :
+		pass
+
+
+T=threading.Thread		
+
+for n in range(1,1000,20):
+	
+	while threading.active_count()>20:
+		
+		continue
+	
+	t=T(target=spider,args=(n,))
+	t.start()			

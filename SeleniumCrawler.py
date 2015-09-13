1f650b1b4f2bfbd6f66ffc782fa9fd623b177b7b
devarajphukan <devaraj.phukan@gmail.com>
2015-09-13 20:16:18 +0530

    Update SeleniumCrawler.py

--- SeleniumCrawler.py
+++ SeleniumCrawler.py
@@ -5,40 +5,37 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-fo = open("websiteLinks","r")
+fo = open("websites","r")
 fw = open("data.txt","a")
 
 driver = webdriver.Firefox()
-timer = 10
+
 for y in fo :
 	
 	time.sleep(5)
 	
-	
-	
 	print y
 	driver.get("http://www.google.com/"+chr(35)+"q="+str(y))
 	source_code = driver.page_source
-		
 	soup = BeautifulSoup(source_code,"lxml")
-
+	
 	c = 1
 	
 	try :
 		i = soup.find('span',{'class':'st'}).text
-		
 		i=str(filter(lambda x:ord(x)>31 and ord(x)<128,i))
 		main_text = i
 		fw.write(main_text)
 		fw.write("\n")
+	
 	except :
 		pass
 
 	
 	try :	
 		for i in soup.find_all('div',{'class':'st'}):
-			j = i.text
 			
+			j = i.text
 			j=str(filter(lambda x:ord(x)>31 and ord(x)<128,j))
 			fw.write(j)
 			fw.write("\n")
@@ -47,15 +44,15 @@ for y in fo :
 	except :
 		pass
 		
-
-	
-
 	d = 0
+	
 	try :
 		
 		for i in soup.find_all('h3',{'class':'r'}):
+			
 			if d == c :
 				break
+			
 			else :
 				j = i.find('a').get('href')
 				j=str(filter(lambda x:ord(x)>31 and ord(x)<128,j))
@@ -76,7 +73,6 @@ for y in fo :
 	
 	
 	fw.write("\n\n")
-	timer += 1
 
 
 	

7b40b6683671de4908c1695581a247a4fc9625d6
devarajphukan <devaraj.phukan@gmail.com>
2015-09-16 01:51:17 +0530

    Update pixabayAsync.py

--- pixabayAsync.py
+++ pixabayAsync.py
@@ -8,27 +8,29 @@ def get(*a, **k):
 	response = yield from aiohttp.request("GET", *a, **k)
 	return (yield from response.text())
 
-
 def spider(page):
 	
 	soup = BeautifulSoup(page,'lxml')
 	mylinks = []
 	mytags = []
+	
 	for link in soup.findAll('img',{'class' : 'preview_img'}):
 		image_link = "https://pixabay.com" + link.get('data-url')
 		tags = link.get('alt')
 		mylinks.append(image_link)
 		mytags.append(tags)
-	info = zip(mylinks,mytags)
-	info = list(info)
+	
+	info = list(zip(mylinks,mytags))
 
 	return info
 
 def print_link_tags(page_num):	
 	
 	url = "https://pixabay.com/en/photos/?&pagi={}".format(page_num)
+	
 	with (yield from sem):
 		page = yield from get(url, compress=True)
+	
 	links = spider(page)
 	
 	for i in range(len(links)):

85f49777811b6af7c5c303e34516bfad0797076d
Devaraj Phukan <devaraj.phukan@gmail.com>
2015-11-18 05:26:17 +0530

    Updated xls writer

--- amazon_all_parts.py
+++ amazon_all_parts.py
@@ -1,44 +1,55 @@
-
 import requests
 from bs4 import BeautifulSoup
+from xlutils.copy import copy
+from xlrd import *
+import threading
+w = copy(open_workbook('data.xls'))
 fw = open("amazonPartLinks.txt","r")
- 
+a = 1
+links = []
+
 for i in fw:
+	links.append(i)
 
-
-
-	url = i.strip()
+def getInfo(i) :
+	url = links[i].strip()
 	try :
 		html = requests.get(url).text
-
-		infoDic = {}
-		
+		product_info = {}
 		soup = BeautifulSoup(html,"lxml")
-
 		name = soup.find("span",{"id":"productTitle"}).text
-		infoDic["name"] = str(name).strip()
-		infoDic["url"] = url
+		product_info["name"] = str(name).strip()
+		product_info["url"] = url
 
 		try :
 			price = soup.find("span",{"id":"priceblock_ourprice"})
-			infoDic["price"] = str(price).split("</span>")[-2].strip()
+			product_info["price"] = str(price).split("</span>")[-2].strip()
 		except :
-			
-			continue	
+			pass	
 
 		j = soup.find("div",{"class":"pdTab"}).find("table").find("tbody")
-		
+		elem = "<br>"
 		for k in j.find_all("tr") :
 			l = k.find_all("td")
 			try :
-
-				infoDic[str(l[0].text).strip()] = str(l[1].text).strip()
+				elem = elem + str(l[0].text).strip() + " : " + str(l[1].text).strip() + "<br>"
 			except :
 				pass
 
+		print product_info["name"]
+		w.get_sheet(0).write(a,2,product_info["name"])
+		w.get_sheet(0).write(a,4,product_info["price"])
+		w.get_sheet(0).write(a,15,product_info["url"])
+		w.get_sheet(0).write(a,13,elem)
+		w.save('amazonAllPart.xls')
+		a += 1
 		
-		print infoDic
-	
 	except :
+		pass
 
-		continue 
+T=threading.Thread		
+for m in range(1,len(links)) :
+	while threading.active_count()>10 :
+		continue
+	t=T(target=getInfo,args=(m,))
+	t.start()
